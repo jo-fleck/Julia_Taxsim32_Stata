db26c444bc7adcf4ccca3d90b012637a5af78202
Johannes Fleck <jofleck.work@gmail.com>
2025-01-17 22:36:37 -0500

    Up

--- Julia_Taxsim32_Stata.jl
+++ Julia_Taxsim32_Stata.jl
@@ -14,7 +14,7 @@
 ## Structure
 
 # 1. Instructions
-# 2. Function Definition: taxsim32_stata
+# 2. Function Definition: taxsim32_stata.jl
 # 3. Example
 
 
@@ -30,13 +30,13 @@
 # PC: point the command to the Stata exe file (I guess...)
 
 
-## 2. Function Definition: taxsim32_stata
+## 2. Function Definition: taxsim32_stata.jl
 
 function taxsim32_stata(dir, df) # (version: 1.0.0)
 
 CSV.write(dir * "taxsim32_input.csv", df); # Save Taxsim32 input as csv
 
-# Generate intermediate Stata do file: Loads taxsim32 input data, calls taxsim32.ado, saves taxsim32 output data
+# Generate temporary Stata do file: loads taxsim32 input data, calls taxsim32.ado, saves taxsim32 output data
 tmp_do_file = ["clear all", "cd " * dir, "import delimited using " * dir * "taxsim32_input.csv", "taxsim32, full", "use " * dir * "taxsim_out.dta, clear", "export delimited " * dir * "taxsim32_output.csv, replace"]
 outfile = dir * "tmp_do_file.do"
 open(outfile, "w") do f
@@ -49,14 +49,14 @@ end
 command_taxsim32_do = `stata-se -b do $outfile`;
 process_taxsim32_do = run(command_taxsim32_do, wait = true);
 
-df_taxsim32 = CSV.read(dir * "taxsim32_output.csv", DataFrame); # import Taxsim32 output
+df_taxsim32_output = CSV.read(dir * "taxsim32_output.csv", DataFrame); # import Taxsim32 output
 
 # Avoid cluttering: Remove tmp_do_file, csvs and files created by taxsim32.ado
 rm(dir * "tmp_do_file.do"); rm(dir * "taxsim32_input.csv"); rm(dir * "taxsim32_output.csv");
 rm(dir * "taxsim_out.dta"); rm(dir * "txpydata.raw"); rm(dir * "results.raw");
-# rm("/Users/main/tmp_do_file.log"); # generated in main dir by taxsim32.ado
+rm("/Users/main/tmp_do_file.log"); # generated in main dir by taxsim32.ado
 
-  return df_taxsim32
+  return df_taxsim32_output
 
 end
 
